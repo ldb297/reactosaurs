2eb6ff11c747fba5ad1dd130f0e94b3e1d348fb9
Leonidas <ldbrooks297@gmail.com>
2021-02-24 16:23:03 -0800

    Comment.js: introduced disappearing modal!

--- src/Comment.js
+++ src/Comment.js
@@ -1,10 +1,41 @@
-function Comment(props){        
-    let content = ['What a great app!', 'This app stinks!', 'What\'s an app?', 'I love React!', 'What\'s JSX?', 'Did I do this correctly?', 'How many comments are there?', 'If you give a mouse a cookie', 'One plus One is...Two?']
+import React, { Component } from 'react'
+import Modal from './Modal'
+class Comment extends Component {  
+    state = {
+        content: ['What a great app!', 'This app stinks!', 'What\'s an app?', 'I love React!', 'What\'s JSX?', 'Did I do this correctly?', 'How many comments are there?', 'If you give a mouse a cookie', 'One plus One is...Two?'],
+        show: false
+    }      
+
+    showModal = e => {
+        this.setState({
+            show:true
+        })
+    }
+
+    changeComment = (e) => {
+        this.setState({
+            content: this.state.content.push(e.target.value)
+        })
+    }
+
+    render() {    
+
+        let comments = this.state.content.map((comment) =>{
+            return <p>{comment}</p>
+        })
+
         return(
             <div>
-                {content[Math.floor(Math.random()*content.length)]}<br/>
+                    <p>{comments}</p>
+                {/* {this.state.content[Math.floor(Math.random()*this.state.content.length)]}<br/> */}
+                <button onClick={e=>{
+                    this.showModal()
+                }}
+                > show Modal</button>
+                <Modal show={this.state.show}/>
             </div>
         )
+    }
 }
 
 export default Comment;
